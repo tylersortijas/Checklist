8e2753c375fc29833e02dd7b24aea60057318d6c
tylersortijas <tylersortijas@gmail.com>
2022-09-01 18:14:34 -0700

    First initial commit

---
+++ todolist/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser"); 
+
+const app = express();
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+    const today = new Date();
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {listTitle: day, newListItems: items});
+});
+
+app.post("/", function (req, res){
+    console.log(req.body);
+    let item = req.body.newItem;
+
+    if(req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+
+
+app.listen(3000, function () {
+    console.log("Server has started up in port 3000");
+});
