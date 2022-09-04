fae5941f6937d0e311d7f8cef632f598ae7a9b79
tylersortijas <tylersortijas@gmail.com>
2022-09-03 22:17:21 -0700

    added a template for the date function

--- todolist/app.js
+++ todolist/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { urlencoded } = require("body-parser"); 
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -13,15 +13,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-    const today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
+    let day = date.getDay();
 
     res.render("list", {listTitle: day, newListItems: items});
 });
